a04a3b44a845a12a5e4c876daa087d56d966ea5c
Renan Batista <renanbatistarg@gmail.com>
2020-11-11 15:41:04 -0300

    Mensagem de boa noite

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,7 @@ public class Program {
 	public static void main(String[] args) {
 		System.out.println("Good Morning!");
 		System.out.println("Good Afternoon");
-
+		System.out.println("Good Night");
 	}
 
 }
